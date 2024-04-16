477aac3c5a81fc7644d64b29ef17e7a051df8569
Dank <dang.hoang@vucar.com>
2024-04-16 17:36:48 +0700

    Added plotting functionalities

--- Calender.py
+++ Calender.py
@@ -1,4 +1,9 @@
 import streamlit as st
+from plot import create_calender_plot
 
-st.title('Hello', anchor=False)
-st.subheader('Hi', anchor=False)
+st.set_page_config(layout="wide",
+                   page_title='Calendar')
+
+plot = create_calender_plot()
+st.plotly_chart(plot)
+
