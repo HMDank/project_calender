06857bd4a248295a9e232277c34a04de15646683
Dank <dang.hoang@vucar.com>
2024-04-17 13:04:20 +0700

    Added new_task functionality

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 import streamlit as st
-
-import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
 from plot import create_calender_plot
 from st_pages import show_pages_from_config, hide_pages
-    
+
 st.set_page_config(layout="wide",
                    page_title='Calendar',)
 hide_pages(["Back"])
 show_pages_from_config()
-
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
 
 with st.sidebar:
     st.subheader('Group Status', anchor=False)
-    st.metric('d', 'Dank', label_visibility="collapsed", delta='Active')
-    st.metric('m', 'Moi', label_visibility="collapsed", delta='Active')
-    st.metric('h', 'Hint', label_visibility="collapsed", delta='Free', delta_color='off')
-    st.metric('l', 'Lam', label_visibility="collapsed", delta='Away', delta_color='inverse')
-
+    for username, info in config['credentials']['usernames'].items():
+        if info['status'] == 'Free':
+            st.metric('d', f"{info['name']}", label_visibility="collapsed", delta=f'{info["status"]}', delta_color='off')
+        elif info['status'] == 'Active':
+            st.metric('d', f"{info['name']}", label_visibility="collapsed", delta=f'{info["status"]}', delta_color='normal')
+        else:
+            st.metric('d', f"{info['name']}", label_visibility="collapsed", delta=f'{info["status"]}', delta_color='inverse')
 st.header('Group Schedule', anchor=False)
 
 st.header('Group Progress', anchor=False)
 
 plot = create_calender_plot()
 st.plotly_chart(plot, use_container_width=True)
-
--- test.py
+++ test.py
@@ -5,6 +5,7 @@ data_df = pd.DataFrame(
     {
         "Week": ['1', '2', '3', '4'],
         "busy": [False, False, False, False],
+        'progress':[1,2,3,4]
     }
 )
 
@@ -14,7 +15,13 @@ st.data_editor(
         "busy": st.column_config.CheckboxColumn(
             "Busy?",
             default=False,
-        )
+        ),
+        "progress": st.column_config.ProgressColumn(
+            "Progress",
+            format="%f",
+            min_value=0,
+            max_value=10,
+        ),
     },
     disabled=["widgets"],
     hide_index=True,
