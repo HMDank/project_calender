2a612e927606a234fbeddc25c7957faebd2b48e2
Hnam08112005 <pnhnam0811@gmail.com>
2024-04-24 22:42:15 +0700

    database added

--- database.py
+++ database.py
@@ -16,8 +16,34 @@ def retrieve_user_data(users):
     return final_list
 
 
-def update_tasks():
-    return None
+def retrieve_tasks(task_collections):
+    client = pymongo.MongoClient(st.secrets['mongo']['uri'])
+    db = client['Tasks']
+    final_list = []
+    for task_collection in task_collections :
+        collection = db[task_collection]
+        data = collection.find()
+        final_list.append(list(data))
+    client.close()
+    return final_list
+
+
+
+def update_tasks(task_collection, completed_status, dealine, participants_list, progress, task_name):
+    client = pymongo.MongoClient(st.secrets['mongo']['uri'])
+    
+    db = client['Tasks']
+    collection = db[task_collection]
+    collection.replace_one(
+        {'name': task_name},
+        {
+          'name': task_name,
+          'completed': completed_status,
+          'participants': participants_list,
+          'progress': progress,
+        }
+    )
+    client.close()
 
 
 def update_user(user, status, schedule):
