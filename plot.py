e4ddb63e4ac691a3072a913af6c6213e2cc2233f
Dank <dang.hoang@vucar.com>
2024-04-25 13:26:57 +0700

    Fixed calendar function

--- plot.py
+++ plot.py
@@ -7,8 +7,8 @@ from yaml.loader import SafeLoader
 from datetime import datetime, timedelta, date
 
 
-def create_calender_plot():
-    dummy_df = create_schedule_table(st.session_state['busy_timeframe'])
+def create_calender_plot(busy_list):
+    dummy_df = create_schedule_table(busy_list)
 
     fig = calplot(dummy_df,
                   name='Busy People',
@@ -28,9 +28,8 @@ def create_calender_plot():
 
 
 def merge_overlapping_periods(periods):
-    sorted_periods = sorted(periods)
-    merged_periods = [sorted_periods[0]]
-    for current_start, current_end in sorted_periods[1:]:
+    merged_periods = [periods[0]]
+    for current_start, current_end in periods[1:]:
         last_merged_start, last_merged_end = merged_periods[-1]
         if current_start <= (last_merged_end + timedelta(1)):
             new_start = min(last_merged_start, current_start)
@@ -76,3 +75,52 @@ def datetime_to_str(dt):
     if isinstance(dt, date):
         return dt.isoformat()
     return dt
+
+
+def str_to_datetime(list_of_lists):
+    converted_lists = []
+
+    for inner_list in list_of_lists:
+        converted_inner_list = []
+        for date_str in inner_list:
+            date_obj = datetime.strptime(date_str, '%Y-%m-%d').date()
+            converted_inner_list.append(date_obj)
+        converted_lists.append(converted_inner_list)
+
+    return converted_lists
+
+
+def create_dataframe(users_data):
+    start_date = date.today()
+    end_date = date(2024, 12, 31)
+    date_range = [start_date + timedelta(days=i) for i in range((end_date - start_date).days + 1)]
+
+    # Initialize an empty DataFrame
+    columns = ['Name'] + [date.strftime('%Y-%m-%d') for date in date_range]
+    df = pd.DataFrame(columns=columns)
+    user_rows = []
+    # Loop through users_data and populate the DataFrame
+    for user_data in users_data:
+        name = user_data[0]['name']
+        schedule = user_data[0]['schedule']
+
+        user_row = {'Name': name}
+
+        for day in date_range:
+            user_row[day.strftime('%Y-%m-%d')] = 'Free'
+
+        for busy_range in schedule:
+            start_date_str = busy_range[0]
+            end_date_str = busy_range[1]
+            start_date = datetime.strptime(start_date_str, '%Y-%m-%d').date()
+            end_date = datetime.strptime(end_date_str, '%Y-%m-%d').date()
+            current_date = start_date
+            while current_date <= end_date:
+                user_row[current_date.strftime('%Y-%m-%d')] = 'Busy'
+                current_date += timedelta(days=1)
+        user_rows.append(pd.DataFrame([user_row]))
+
+    df = pd.concat(user_rows, ignore_index=True)
+    df.set_index('Name', inplace=True)
+    return df
+
